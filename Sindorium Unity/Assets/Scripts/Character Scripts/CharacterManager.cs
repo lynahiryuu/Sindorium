ca7ff830dc56bbbd16d1519f5beed61c6d27dfb4
Unknown <lysa.gramoli@laposte.net>
2020-06-22 19:01:30 +0200

    Begin project

---
+++ Sindorium Unity/Assets/Scripts/Character Scripts/CharacterManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour {
+    // Used to manage character
+
+    // Character Camera
+    [SerializeField]
+    private Camera MainCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
