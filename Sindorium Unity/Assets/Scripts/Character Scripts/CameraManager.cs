469eb0c0a470b149928fed7768feb565ad2b833f
Unknown <lysa.gramoli@laposte.net>
2020-06-28 20:10:40 +0200

    Camera and positions

---
+++ Sindorium Unity/Assets/Scripts/Character Scripts/CameraManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+  
+    [SerializeField]
+    float rotationSpeedCamera = 50f;
+   
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+    // Camera calculation must be made after all the updates
+	void LateUpdate ()
+    {
+        TurnCamera();
+
+	}
+    // Up and Down are modified but not the other because it's already modified in CharacterManager with move and Forward
+    // which modifies the position/rotation of the camera's parent
+    private void TurnCamera()
+    {
+        float pitch = Input.GetAxis("Pitch") * rotationSpeedCamera * Time.deltaTime;
+
+        transform.Rotate(-pitch, 0.0f, 0.0f);
+    }
+}
--- Sindorium Unity/Assets/Scripts/Character Scripts/CharacterManager.cs
+++ Sindorium Unity/Assets/Scripts/Character Scripts/CharacterManager.cs
@@ -5,17 +5,40 @@ using UnityEngine;
 public class CharacterManager : MonoBehaviour {
     // Used to manage character
 
-    // Character Camera
     [SerializeField]
-    private Camera MainCamera;
+    private float speed = 5f;
+    //private float jump = 0.5f;
+    [SerializeField]
+    private float rotationSpeed;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        rotationSpeed = 10 * speed;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        MovePlayer();
+        TurnPlayer();
 	}
+
+    private void MovePlayer()
+    {
+        /* Update the character position when he walks, transform.forward is the forward vector of the character
+        and Input.GetAxis manages buttons in the Input settings of Unity.*/
+        transform.position += new Vector3(transform.forward.x * Input.GetAxis("Vertical")* Time.deltaTime * speed, 0.0f, transform.forward.z * Input.GetAxis("Vertical") * Time.deltaTime * speed); 
+    }
+
+    /* Used to turn the player, the camera will automatically follow this turning. Input.getAxis is managed in the Input Settings of Unity
+     where buttons are associated. */
+    private void TurnPlayer()
+    {
+        float yaw = Input.GetAxis("Yaw") * rotationSpeed * Time.deltaTime;
+
+        transform.Rotate(0.0f, yaw, 0.0f);
+    }
+
+
 }
