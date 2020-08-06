ca624d9242dba446baf28792698f3b4cb1d33f98
Unknown <lysa.gramoli@laposte.net>
2020-08-06 15:03:33 +0200

    Trigger End

--- Sindorium Unity/Assets/Scripts/Actions/TriggerManager.cs
+++ Sindorium Unity/Assets/Scripts/Actions/TriggerManager.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class TriggerManager : MonoBehaviour {
 
-    [SerializeField]
-    private GameObject GateTriggered;
+    // Gate which will be opened with the lever
+    [SerializeField] private GameObject GateTriggered;
     private GameObject Trigger;
-    private bool GateOpened;
+    // Part of the gate which will be translated to open or close the door
+    //private Rigidbody PartOfGateMoving;
+    private GameObject PartOfGateMoving;
 
-    [SerializeField]
-    private int IDGate;
+    private bool TriggerFound = false;
+    private bool DoorOpened = false;
 
+    private float TriggerAngleRotation = 90;
+
+    private float GateTranslation = 2.5f;
 
     // Use this for initialization
     void Start () {
@@ -23,13 +28,70 @@ public class TriggerManager : MonoBehaviour {
             if (child.tag == "Trigger")
             {
                 Trigger = child.gameObject;
-                //Debug.Log("Child found");
+                Debug.Log("Trigger Child found");
+            }
+        }
+
+        //Looking for the rigidBody of the attached door to move it later
+        foreach(Transform child in GateTriggered.transform)
+        {
+            if(child.tag == "Trigger")
+            {
+                //PartOfGateMoving = child.gameObject.GetComponent<Rigidbody>();
+                PartOfGateMoving = child.gameObject;
+                Debug.Log("Gate Child Found");
             }
         }
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        // If the player presses E when the door is opened, then we close the door
+		if(TriggerFound && Input.GetButtonDown("Interact") && DoorOpened)
+        {
+            TriggerDoor(-1);
+            DoorOpened = false;
+        }
+
+        // If the player presses E when the door is closed, then we open the door
+        else if (TriggerFound && Input.GetButtonDown("Interact") && !DoorOpened)
+        {
+            TriggerDoor(1);
+            DoorOpened = true;
+        }
 	}
+
+    // Open or close the door when the player pushes the lever.
+    // state parameter = 1 for opening and -1 for closing
+    private void TriggerDoor(int TriggerState)
+    {
+        // Change the position of the lever
+        float Xrotation = TriggerState * TriggerAngleRotation;
+        Trigger.transform.Rotate(Xrotation, 0.0f, 0.0f);
+
+        // Change the position of the gate
+        float YTranslation = TriggerState * GateTranslation;
+       // PartOfGateMoving.AddForce(0.0f, YTranslation, 0.0f);
+        PartOfGateMoving.transform.position += new Vector3(0.0f, YTranslation , 0.0f); 
+    }
+
+    //Collision test to know whether or not the player is close to the lever.
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerBody")
+        {
+            TriggerFound = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "PlayerBody")
+        {
+            TriggerFound = false;
+        }
+    }
+
+
 }
